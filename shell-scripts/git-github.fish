55aaf651819018558c574815a3bcf39b21dcf2e7
muhmdtaha <mohamedengineer944@gmail.com>
2022-04-29 00:36:54 +0200

    adding git and github aliases

---
+++ shell-scripts/git-github.fish
@@ -0,0 +1,15 @@
+# git
+alias addup='git add -u'
+alias addall='git add .'
+alias branch='git branch'
+alias checkout='git checkout'
+alias clone='git clone'
+alias commit='git commit -m'
+alias fetch='git fetch'
+alias pull='git pull origin'
+alias push='git push origin'
+alias tag='git tag'
+alias newtag='git tag -a'
+
+######## git bare repository ########
+alias config="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
