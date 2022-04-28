9146b2aea449cd0070a38d58b8909308c78c42d4
muhmdtaha <mohamedengineer944@gmail.com>
2022-04-29 00:38:26 +0200

    adding vim and emacs aliases

---
+++ shell-scripts/vim-emacs.fish
@@ -0,0 +1,8 @@
+# vim and emacs
+alias vim='nvim'
+alias em='/usr/bin/emacs -nw'
+alias emacs="emacsclient -c -a 'emacs'"
+alias doomsync="~/.emacs.d/bin/doom sync"
+alias doomdoctor="~/.emacs.d/bin/doom doctor"
+alias doomupgrade="~/.emacs.d/bin/doom upgrade"
+alias doompurge="~/.emacs.d/bin/doom purge"
